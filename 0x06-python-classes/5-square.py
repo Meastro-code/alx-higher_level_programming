3668438b2fc3008238c750657e8fb6012911b269
Meastro-code <codemaestro87@gmail.com>
2023-12-26 10:44:03 -0800

    5-square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""
+A class square that defines a square
+"""
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        area(self): Returns the area of the square.
+        size(self): Getter method to retrieve the size of the square.
+        size(self, value): Setter method to set the size of the square.
+        my_print(self): Prints the square with the character #.
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the area of the square.
+
+        Args:
+            None
+
+        Returns:
+            The area of the square.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size of the square.
+
+        Args:
+            None
+
+        Returns:
+            The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Returns:
+            None
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """
+        Prints the square with the character #.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
