833b07f88cfa3a4fd1a52127e0e323568d9e7099
Meastro-code <codemaestro87@gmail.com>
2023-12-26 10:23:43 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-'''
+"""
    An empty square class
-'''
+"""
 class Square:
     """
     This class represents a square.
 
     Attributes:
+        None
+
+    Methods:
         None
     """
     pass
