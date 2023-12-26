ee7e691569207f7e5853ae814534012c22a438d1
Meastro-code <codemaestro87@gmail.com>
2023-12-26 11:06:29 -0800

    101-square

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+"""
+A class Square that defines a square
+"""
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+        __position (tuple): The position of the square.
+
+    Methods:
+        __init__(self, size=0, position=(0, 0)): Initializes a new instance of the Square class.
+        area(self): Returns the area of the square.
+        size(self): Getter method to retrieve the size of the square.
+        size(self, value): Setter method to set the size of the square.
+        position(self): Getter method to retrieve the position of the square.
+        position(self, value): Setter method to set the position of the square.
+        my_print(self): Prints the square with the character #.
+        __str__(self): Returns a string representation of the square.
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """
+        Returns the area of the square.
+
+        Args:
+            None
+
+        Returns:
+            The area of the square.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size of the square.
+
+        Args:
+            None
+
+        Returns:
+            The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Returns:
+            None
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """
+        Getter method to retrieve the position of the square.
+
+        Args:
+            None
+
+        Returns:
+            The position of the square.
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        Setter method to set the position of the square.
+
+        Args:
+            value (tuple): The position of the square.
+
+        Returns:
+            None
+        """
+        if not isinstance(value, tuple) or len(value) != 2 or \
+                not isinstance(value[0], int) or not isinstance(value[1], int) or \
+                value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """
+        Prints the square with the character #.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+
+        Args:
+            None
+
+        Returns:
+            A string representation of the square.
+        """
+        return self.my_print()
