395ad67fc92c4e519e31c609611a1f4ab11bf5da
Meastro-code <codemaestro87@gmail.com>
2023-12-26 10:28:00 -0800

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A Square class
+"""
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        None
+    """
+    def __init__(self, size):
+        self.__size = size
