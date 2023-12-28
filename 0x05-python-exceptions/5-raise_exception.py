1527a3e1dcc5ffede5830aa8f26b47058b5946b8
Meastro-code <codemaestro87@gmail.com>
2023-12-28 13:55:27 -0800

    5-raise_exception

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def raise_exception():
-  # This function will raise a TypeError when called
-  # It tries to add an integer and a string, which is not allowed in Python
   x = 1
   y = "2"
   z = x + y
