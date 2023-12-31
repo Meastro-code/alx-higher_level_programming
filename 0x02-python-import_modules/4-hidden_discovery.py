9be3367bf2e5c3add0b7f8bbaff2bfa5dcf86556
Meastro-code <codemaestro87@gmail.com>
2023-12-31 08:35:25 -0800

    4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.8
 import hidden_4
+
 if __name__ == "__main__":
-    for i in dir(hidden_4):
-        if not i.startswith("__"):
-            print("{}".format(i))
+    for name in dir(hidden_4):
+        if not name.startswith("__"):
+            print(name)
