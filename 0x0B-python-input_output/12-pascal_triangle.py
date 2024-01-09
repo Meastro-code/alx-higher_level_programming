5400427e63ab91d092cc9fe58611ce016e9b1e3a
Meastro-code <codemaestro87@gmail.com>
2024-01-09 15:05:48 -0800

    12-pascal_triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+   function def pascal_triangle(n)
+"""
+def pascal_triangle(n):
+    """ function to create pascal triangle """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
