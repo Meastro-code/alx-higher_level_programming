4525d0971dc539e41316b3b9b086d15895863831
Meastro-code <codemaestro87@gmail.com>
2024-01-09 14:00:56 -0800

    2-append_write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+   function to create and append on file
+"""
 def append_write(filename="", text=""):
+    """ fucntion to append or create """
     with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
