aeab8c84d3c95d0905b87659752cbf3b237c270d
Meastro-code <codemaestro87@gmail.com>
2024-01-09 14:02:53 -0800

    1-write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+   function to write on file
+"""
 def write_file(filename="", text=""):
     """ function to write a file """
     with open(filename, mode='w', encoding='utf-8') as f:
