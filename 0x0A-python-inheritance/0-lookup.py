4f2735bedfa87cf99919c3675e518eb8b1e9ff57
Meastro-code <codemaestro87@gmail.com>
2024-01-10 13:00:18 -0800

    0-lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,4 +4,4 @@
 """
 def lookup(obj):
     """ function that returns lists of attributes and methods of obj """
-    return [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
+    return [attr for attr in dir(obj)]
