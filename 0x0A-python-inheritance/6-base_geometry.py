402bf1a2c710952da4989bd25dbc250f533bd2e0
Meastro-code <codemaestro87@gmail.com>
2024-01-10 13:37:27 -0800

    6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+   Class basegeometry 
+"""
+class BaseGeometry:
+    def area(self):
+        """ function to calculate area """
+        raise Exception("area() is not implemented")
