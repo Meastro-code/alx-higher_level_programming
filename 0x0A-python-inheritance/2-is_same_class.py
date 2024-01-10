4931c8cc19ebbff7e9f460d65c972ae43377c11a
Meastro-code <codemaestro87@gmail.com>
2024-01-10 13:16:33 -0800

    2-is_same_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+  Function to check if object is an exact instance
+"""
+def is_same_class(obj, a_class):
+    """ Function to check if obj is an exact instance """
+    return type(obj) is a_class
