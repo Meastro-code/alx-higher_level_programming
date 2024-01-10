641eb688842e04ae32fdf1cb78102c9d317c5610
Meastro-code <codemaestro87@gmail.com>
2024-01-10 12:45:39 -0800

    101-add_attribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,7 @@
    Function to add attribute
 """
 def add_attribute(obj, name, value):
+    """ function to add attribute """
     if hasattr(obj, name):
         raise TypeError("can't add new attribute")
     else:
