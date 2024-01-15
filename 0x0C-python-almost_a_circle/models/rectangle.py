4eb645513216c50dba9bdc08c59bca81f6382829
Meastro-code <codemaestro87@gmail.com>
2024-01-15 15:41:21 -0800

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -71,7 +71,7 @@ class Rectangle(Base):
         return self.width * self.height
 
     def display(self):
-        """ func display """
+        """ func dispaly """
         for i in range(self.y):
             print()
         for i in range(self.height):
@@ -80,5 +80,18 @@ class Rectangle(Base):
             print("#" * self.width)
 
     def __str__(self):
-        """ func string obj """
+        """ func str """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args):
+        """ func update """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
