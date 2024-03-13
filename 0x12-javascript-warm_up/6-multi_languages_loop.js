57da6633796eaffacd9dfd778cd198a3fe3e9e65
Meastro-code <codemaestro87@gmail.com>
2024-03-12 17:06:21 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Create an array of strings
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Print each line using a loop
+for (const line of lines) {
+  console.log(line);
+}
