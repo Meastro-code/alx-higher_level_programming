8e68b7ea53af3fa9c08fde74d614ec67ef889c71
Meastro-code <codemaestro87@gmail.com>
2024-03-12 18:05:30 -0700

    13-add.js

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/**
+ * Adds two integers and returns the result.
+ * @param {number} a - First integer.
+ * @param {number} b - Second integer.
+ * @returns {number} - Sum of a and b.
+ */
+function add (a, b) {
+  return a + b;
+}
+
+module.exports = {
+  add
+};
