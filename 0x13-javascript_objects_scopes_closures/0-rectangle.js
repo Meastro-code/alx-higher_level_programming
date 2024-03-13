eef645464f0d6b6ae920f477f2b638f60582cc16
Meastro-code <codemaestro87@gmail.com>
2024-03-13 16:03:44 -0700

    0-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor () {
+    // Initialize any properties you need here
+    // For an empty Rectangle, you don't need to set any properties
+  }
+
+  // You can add other methods or properties to the Rectangle class as needed
+  // For example:
+  // calculateArea() {
+  //     return 0; // Since it's an empty rectangle
+  // }
+}
