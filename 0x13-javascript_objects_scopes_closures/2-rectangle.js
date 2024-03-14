22d116eb4646b2ebd5e4e27ce1c9e6dcedb7441f
Meastro-code <codemaestro87@gmail.com>
2024-03-13 17:13:54 -0700

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || h=== undefined || w===undefined) {
+    if (w <= 0 || h <= 0 || h === undefined || w === undefined) {
       // Create an empty object if width or height is not positive
     //  console.log('undefined');
     } else {
